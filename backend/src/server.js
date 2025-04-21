23a0be53e4edb70ee869feb7058180e96ac24e84
Akinde Tosin Michael <tosin.akinde36@gmail.com>
2025-04-21 18:35:23 +0100

    server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const app = express();
+const app = require('./app'); // import the Express app from app.js
 const PORT = process.env.PORT || 3001;
-
-// Default route to handle "/"
-app.get('/', (req, res) => {
-    res.send('Backend is running!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Default route to handle "/"
+// app.get('/', (req, res) => {
+//     res.send('Backend is running!');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
