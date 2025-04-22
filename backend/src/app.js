261a0e5a3a06bb605c93ff032eb731a29ab6e7b5
Akinde Tosin Michael <tosin.akinde36@gmail.com>
2025-04-22 01:15:22 +0100

    backend update2

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,12 +27,12 @@ app.use('/api/payment', paymentRoutes);
 app.use('/payment', paymentRoutes); 
 
 // Serve static files from the build directory
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle all other routes by serving React
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// // Handle all other routes by serving React
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // Global error handler
 app.use((err, req, res, next) => {
